fab34e2ff918e9ba319d062040afda0b8c1ae4aa
tzwm <wangjunzhe4444@gmail.com>
2021-10-29 12:56:22 +0800

    Add lasagna #elixir

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,22 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  @expected_minutes_in_oven 40
+  @preparation_time 2
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def expected_minutes_in_oven, do: @expected_minutes_in_oven
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def remaining_minutes_in_oven(minutes) do
+    ret = expected_minutes_in_oven() - minutes
+    if ret < 0, do: 0, else: ret
+  end
 
-  # Please define the 'total_time_in_minutes/2' function
 
-  # Please define the 'alarm/0' function
+  def preparation_time_in_minutes(number) do
+    number * @preparation_time
+  end
+
+  def total_time_in_minutes(number, minutes) do
+    preparation_time_in_minutes(number) + minutes
+  end
+
+  def alarm, do: "Ding!"
 end
